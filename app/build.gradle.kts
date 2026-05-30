170aa2900ed541e5b2849137d7e6c0e882779169
uniK007 <kislayk970@gmail.com>
2026-05-30 10:05:00 +0530

    Updated: nav graph

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
 }
--- app/src/main/java/com/example/firebaseauthmvvm/MainActivity.kt
+++ app/src/main/java/com/example/firebaseauthmvvm/MainActivity.kt
@@ -4,48 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.firebaseauthmvvm.presentation.login.LoginViewModel
-import com.example.firebaseauthmvvm.ui.theme.FirebaseAuthMVVMTheme
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.compose.runtime.collectAsState
-import com.example.firebaseauthmvvm.presentation.login.LoginScreen
+import com.example.firebaseauthmvvm.presentation.navigation.AppNavGraph
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val viewModel by viewModels<LoginViewModel>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val state = viewModel.state.collectAsState().value
-
-            MaterialTheme {
-                Surface {
-                    LoginScreen(
-                        state = state,
-                        onLoginClick = { email, password ->
-                            viewModel.login(
-                                email, password
-                            )
-                        },
-
-                        onPasswordChanged = {
-                            viewModel.onPasswordChanged(it)
-                        }
-                    )
-                }
-            }
+            AppNavGraph()
         }
     }
 }
